4d0a43dfead32ceec9fa95b6cd9cbb0c72ca2a9e
Garrett Edwards <grrttedwards@users.noreply.github.com>
2022-06-16 20:29:04 -0400

    Add misc debug logging

--- background.js
+++ background.js
@@ -1,9 +1,14 @@
+const waitMs = 5000;
+
 function closeZoomTabOnSuccessListener(tabId, changeInfo, tab) {
   if (tab.url.match(/https:\/\/.+?\.zoom\.us\/.+?success.*/)) {
+    console.debug(`Detected #success, closing tab in ${waitMs} ms`);
     setTimeout(() => {
+      console.debug(`Closed tab ID ${tabId}`);
       chrome.tabs.remove(tabId, _ => chrome.runtime.lastError);
-    }, 5000)
+    }, waitMs)
   }
 }
 
+console.debug("Enabled zoom tab listener");
 chrome.tabs.onUpdated.addListener(closeZoomTabOnSuccessListener);
